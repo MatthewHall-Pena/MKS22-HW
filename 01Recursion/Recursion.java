360caa0e3ef75ca391dad1d2fa982d588c67e26f
MatthewHall-Pena <mhallpena@stuy.edu>
2016-02-08 09:36:27 -0500

    hw01-2

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -5,14 +5,14 @@ public class Recursion {
 	}
 
 	public static double sqrt(double n) {
-		return sqrt1(n, n / 2);
+		return sqrt1(n, n);
 	}
 
 	public static double sqrt1(double n, double guess) {
 		if (n < 0) {
 			throw new IllegalArgumentException();
 		}
-		if (Math.abs(((guess * guess) + 1) / (n + 1) - 1) < .01) {
+		if (Math.abs(((guess * guess) + 1) / (n + 1) - 1) < .0000000000001) {
 			return guess;
 		}
 		return sqrt1(n, (n / guess + guess) / 2);
