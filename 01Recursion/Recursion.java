acfa9401cdabfb0d2d8707ecc9af308c112f2e23
MatthewHall-Pena <mhallpena@stuy.edu>
2016-02-07 20:58:02 -0500

    hw1-1

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -1,21 +1,20 @@
-import java.lang.IllegalArgumentException;
-public class Recursion{
-    public String name(){
-	return "Hall-Pena,Matthew";
-    }
-    public static double sqrt(double n){
-	return sqrt1(n,n/2);
-    }
-    public double sqrt1(double n,double guess){
-	/*if(n<0){
-	    throw IllegalArgumentException();
-	    }*/
-	if(Math.abs(guess*guess/n-1)<.000001){
-	    return guess;
+
+public class Recursion {
+	public String name() {
+		return "Hall-Pena,Matthew";
 	}
-	return sqrt1(n,( n / guess + guess) / 2);
-    }
-    public void main(String[] args){
-	
-    }
+
+	public static double sqrt(double n) {
+		return sqrt1(n, n / 2);
+	}
+
+	public static double sqrt1(double n, double guess) {
+		if (n < 0) {
+			throw new IllegalArgumentException();
+		}
+		if (Math.abs(((guess * guess) + 1) / (n + 1) - 1) < .01) {
+			return guess;
+		}
+		return sqrt1(n, (n / guess + guess) / 2);
+	}
 }
