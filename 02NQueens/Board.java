a2753b3bc206667ea373cadd8f40b0da7686a547
MatthewHall-Pena <mhallpena@stuy.edu>
2016-02-09 20:16:15 -0500

    hw2-0

---
+++ 02NQueens/Board.java
@@ -0,0 +1,85 @@
+
+public class Board {
+	int[][] board;
+
+	public Board(int n) {
+		board = new int[n][n];
+	}
+
+	public boolean addQ(int r, int c) {
+		if (board[r][c] != 0) {
+			return false;
+		}
+		board[r][c] = 1;
+		for (int x = c + 1; x < board.length; x++) {
+			board[r][x] -= 1;
+		}
+		for (int x = 1; x < board.length - c; x++) {
+			if (r + x < board.length) {
+				board[r + x][c + x] -= 1;
+			}
+			if (r - x > -1) {
+				board[r - x][c + x] -= 1;
+			}
+
+		}
+		return true;
+
+	}
+
+	public boolean removeQ(int r, int c) {
+		if (board[r][c] != 1) {
+			return false;
+		}
+		board[r][c] = 0;
+		for (int x = c + 1; x < board.length; x++) {
+			board[r][x] += 1;
+		}
+		for (int x = 1; x < board.length - c; x++) {
+			if (r + x < board.length) {
+				board[r + x][c + x] += 1;
+			}
+			if (r - x > -1) {
+				board[r - x][c + x] += 1;
+			}
+
+		}
+		return true;
+	}
+
+	public void print() {
+		String s = "";
+		for (int x = 0; x < board.length; x++) {
+			for (int y = 0; y < board.length; y++) {
+				if (board[x][y] == 1) {
+					s += " Q ";
+				} else {
+					s += " " + 0 + " ";
+				}
+			}
+			System.out.println(s);
+			s = "";
+		}
+
+	}
+
+	public boolean solve() {
+
+		return solver(0);
+	}
+
+	public boolean solver(int c) {
+		if (c >= board.length) {
+			return true;
+		}
+		for (int r = 0; r < board.length; r++) {
+			if (addQ(r, c) && solver(c + 1)) {
+				return true;
+			}
+			removeQ(r, c);
+		}
+
+		return false;
+	}
+
+}
