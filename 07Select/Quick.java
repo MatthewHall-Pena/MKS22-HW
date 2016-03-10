309c3651d48a8b7bc2592558a99ade9b8cbc7c9c
MatthewHall-Pena <mhallpena@stuy.edu>
2016-03-10 13:08:49 -0500

    hw08-03

--- 07Select/Quick.java
+++ 07Select/Quick.java
@@ -24,8 +24,12 @@ public static void printArray(int[] data) {
 		Random r = new Random();
 		int index = left + r.nextInt() % ((right - left + 1) / 2) + (right - left + 1) / 2;
 		int split = data[index];
+		int end=right;
 	        swap(data,right,index);
+		right--;
 		while (left != right) {
+		    System.out.println(split+" "+left+" "+right);
+		    printArray(data);
 			if (data[left] > split) {
 			    swap(data,right,left);
 				right --;
@@ -36,31 +40,27 @@ public static void printArray(int[] data) {
 				left ++;
 			}
 		}
+		//swap(data,end,left+1);
 		return left;
 	}
-
-	public static int quickselect(int[] data, int k) {
-		int left = 0;
-		int right = data.length - 1;
-		int index = 0;
-		while (right > k) {
-			index = partition(data, left, right);
-			if (index == right) {
-				right --;
-			} else if (index >= k) {
-				right = index;
-			} else if (index < k) {
-				left = index;
-			}
-		}
-		int max = data[left];
-		for (int x = left + 1; x < right + 1; x++) {
-			if (data[x] > max) {
-				max = data[x];
-			}
-		}
-		return max;
+    public static int quickselect(int[] data, int k,int left,int right) {
+	if(left==right){
+	    return data[k];
 	}
+	int index=partition(data,left,right);
+	System.out.println(left+" "+right);
+	if(index==k){
+	    return data[k];
+	}
+	if(index>k){
+	    return quickselect(data,k,left,index);
+	}
+	return quickselect(data,k,index+1,right);
+    }
+	public static int quickselect(int[] data, int k) {
+	    return quickselect(data,k,0,data.length-1);
+	}
+    
     private static boolean isSorted(int[] data){
 	if(data.length<=1){
 	    return true;
@@ -78,8 +78,8 @@ public static void printArray(int[] data) {
 	}
     }
     public static void main(String[]args){
-	int[] data={2,3,5,1,6,7,10,9,8,0};
-	quickSort(data);
+	int[] data={2,3,5,1,6,7,10,9,8,0,4};
+	System.out.println(quickselect(data,5));
 	printArray(data);
     }
 }
