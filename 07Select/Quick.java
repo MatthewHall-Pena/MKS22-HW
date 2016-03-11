a7e763be43a244420931ea3b9bdf6c1a8dabb7bb
MatthewHall-Pena <mhallpena@stuy.edu>
2016-03-10 21:33:30 -0500

    hw08-04

--- 07Select/Quick.java
+++ 07Select/Quick.java
@@ -1,10 +1,9 @@
-import java.util.Random;
-
 public class Quick {
-	public static String name(){
-		return "7,Hall-Pena,Matthew"; 
-    	}
-public static void printArray(int[] data) {
+	public static String name() {
+		return "7,Hall-Pena,Matthew";
+	}
+
+	public static void printArray(int[] data) {
 		String s = "[ ";
 		for (int x = 0; x < data.length; x++) {
 			s += data[x];
@@ -15,71 +14,78 @@ public static void printArray(int[] data) {
 		s += "]";
 		System.out.println(s);
 	}
-    private static void swap(int[]data,int a, int b){
-	int old = data[a];
-	data[a]=data[b];
-	data[b]=old;
-    }
-	private static int partition(int[] data, int left, int right) {
-		Random r = new Random();
-		int index = left + r.nextInt() % ((right - left + 1) / 2) + (right - left + 1) / 2;
-		int split = data[index];
-		int end=right;
-	        swap(data,right,index);
-		right--;
-		while (left != right) {
-		    System.out.println(split+" "+left+" "+right);
-		    printArray(data);
-			if (data[left] > split) {
-			    swap(data,right,left);
-				right --;
-			} else if (data[right] < split) {
-				swap(data,right,left);
-				left ++;
-			} else {
-				left ++;
+
+	private static void swap(int[] data, int a, int b) {
+		int old = data[a];
+		data[a] = data[b];
+		data[b] = old;
+	}
+
+	private static int max(int[] data, int a, int b) {
+		int max = data[a];
+		for (int x = a + 1; x < b + 1; x++) {
+			if (data[x] > max) {
+				max = data[x];
 			}
 		}
-		//swap(data,end,left+1);
+		return max;
+	}
+
+	private static int partition(int[] data, int left, int right) {
+		int index = (int) (left + Math.random() * (right - left + 1));
+		int split = data[index];
+		int end = right;
+		swap(data, right, index);
+		right--;
+		while (left < right) {
+			if (data[left] >= split && data[right] <= split) {
+				swap(data, right, left);
+				right--;
+			} else if (data[left] > split && data[right] > split) {
+				right--;
+			} else {
+				left++;
+			}
+		}
+		if (data[left] > data[end]) {
+			swap(data, end, left);
+		}
 		return left;
 	}
-    public static int quickselect(int[] data, int k,int left,int right) {
-	if(left==right){
-	    return data[k];
+
+	public static int quickselect(int[] data, int k, int left, int right) {
+		if (left == right) {
+			return max(data, 0, k);
+		}
+		int index = partition(data, left, right);
+		if (index == k) {
+			return max(data, 0, k);
+		}
+		if (index > k) {
+			return quickselect(data, k, left, index);
+		}
+		return quickselect(data, k, index + 1, right);
 	}
-	int index=partition(data,left,right);
-	System.out.println(left+" "+right);
-	if(index==k){
-	    return data[k];
-	}
-	if(index>k){
-	    return quickselect(data,k,left,index);
-	}
-	return quickselect(data,k,index+1,right);
-    }
+
 	public static int quickselect(int[] data, int k) {
-	    return quickselect(data,k,0,data.length-1);
+		return quickselect(data, k, 0, data.length - 1);
 	}
-    
-    private static boolean isSorted(int[] data){
-	if(data.length<=1){
-	    return true;
+
+	public static void quickSort(int[] data) {
+		quickSort(data, 0, data.length - 1);
 	}
-	for(int x=1;x<data.length;x++){
-	    if(data[x-1]>data[x]){
-		return false;
-	    }
+
+	public static void quickSort(int[] data, int left, int right) {
+		if (left < right) {
+			int index = partition(data, left, right);
+			quickSort(data, left, index);
+			quickSort(data, index + 1, right);
+		}
 	}
-	return true;
-    }
-    public static void quickSort(int[] data){
-	while(!isSorted(data)){
-	    partition(data,0,data.length-1);
+
+	public static void main(String[] args) {
+		int[] data = { 0, 0, 2, 0, 0, 0, 0, 1 };
+		quickSort(data);
+		printArray(data);
 	}
-    }
-    public static void main(String[]args){
-	int[] data={2,3,5,1,6,7,10,9,8,0,4};
-	System.out.println(quickselect(data,5));
-	printArray(data);
-    }
 }
