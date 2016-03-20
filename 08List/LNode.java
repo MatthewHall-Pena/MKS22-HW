1cbc4525dfb6ec494de0f3d11a6d0409a95a3dca
MatthewHall-Pena <mhallpena@stuy.edu>
2016-03-20 14:34:34 -0400

    hw10-02

--- 08List/LNode.java
+++ 08List/LNode.java
@@ -1,27 +1,33 @@
-public class LNode{
-    private int value;
-    private LNode next;
-    public LNode(int nValue){
-	value=nValue;
-    }
-    public LNode(int nValue, int LNode nNode){
-	next=nNode;
-	value=nValue;
-    }
-    public int getValue(){
-	return value;
-    }
-    public int setValue(int nValue){
-	int old =value;
-	value=nValue;
-	return old;
-    }
-    public LNode getNext(){
-	return next;
-    }
-    public LNode setValue(LNode nNode){
-	LNode old =next;
-	next=nNode;
-	return old;
-    }
+public class LNode {
+	private int value;
+	private LNode next;
+
+	public LNode(int nValue) {
+		value = nValue;
+	}
+
+	public LNode(int nValue, LNode nNode) {
+		next = nNode;
+		value = nValue;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public int setValue(int nValue) {
+		int old = value;
+		value = nValue;
+		return old;
+	}
+
+	public LNode getNext() {
+		return next;
+	}
+
+	public LNode setNext(LNode nNode) {
+		LNode old = next;
+		next = nNode;
+		return old;
+	}
 }
--- 08List/MyLinkedList.java
+++ 08List/MyLinkedList.java
@@ -1,77 +1,133 @@
-public class MyLinkedList{
-    private LNode start;
-    private int size;
-    public MyLinkedList(){
-    }
-    public MyLinkedList(int nSize){
-	if(nSize>0){
-	    start=new LNode(0);
-	    constructor(nSize,start);
+public class MyLinkedList {
+	private LNode start;
+	private int size;
+
+	public MyLinkedList() {
+		size = 0;
 	}
-    }
-    private void constructor(int nSize,LNode node){
-	if(nSize>0){
-	    LNode Node =new LNode(0);
-	    node.setNext(Node);
-	    constructor(nSize-1,Node);
+
+	public MyLinkedList(int nSize) {
+		if (nSize > 0) {
+			size = nSize;
+			start = new LNode(0);
+			constructor(nSize, start);
+		}
 	}
-    }
-    public boolean add(int value){
-	if(start==null){
-	    start=new LNode(value);
-	    return true;
+
+	private void constructor(int nSize, LNode node) {
+		if (nSize > 0) {
+			LNode Node = new LNode(0);
+			node.setNext(Node);
+			constructor(nSize - 1, Node);
+		}
 	}
-	return add(value,start);
-    }
-    private boolean add(int value, LNode node){
-	if(node.getNext()==null){
-	    node.setValue(value);
-	    return true;
+
+	public boolean add(int value) {
+		if (start == null) {
+			start = new LNode(value);
+			size += 1;
+			return true;
+		}
+		return add(value, start);
 	}
-	return get(value,node.getNext());
-    }
-    public int get(int index){
-	return get(index,start);
-    }
-    private int get(int index, LNode node){
-	if(index==0){
-	    return node.getValue();
+
+	private boolean add(int value, LNode node) {
+		if (node.getNext() == null) {
+			LNode nNode = new LNode(value);
+			node.setNext(nNode);
+			size += 1;
+			return true;
+		}
+		return add(value, node.getNext());
 	}
-	return get(index-1,node.getNext());
-    }
-    public int set(int index,int newValue){
-	return set(index,newValue,start)
-    }
-    private int set(int index,int value, LNode node){
-	if(index==0){
-	    return node.setValue(value);
+
+	public boolean add(int index, int value) {
+		if (index > size || index < 0) {
+			return false;
+		}
+		if (index == size) {
+			return add(value);
+		}
+		return add(index, value, start);
 	}
-	return set(index-1,node.getNext());
-    }
-    public int size(){
-	return size;
-    }
-    public int indexOf(int value){
-	return indexOf(size,value,start);
-    }
-    private int indexOf(int index,int value, LNode node){
-	if(index==0){
-	    return -1;
+
+	private boolean add(int index, int value, LNode node) {
+		if (index == 1) {
+			LNode nNode = new LNode(value, node.getNext());
+			node.setNext(nNode);
+			size += 1;
+			return true;
+		}
+		return add(index - 1, value, node.getNext());
 	}
-	if(node.getValue()==value){
-	    return size-index;
+
+	public boolean remove(int index) {
+		if (index >= size || index < 0) {
+			return false;
+		}
+		return remove(index, start);
 	}
-	return indexOf(index-1,value,node.getNext());
-    }
-    public String toString(){
-	return toString(size,start,"[ ")+" ]";
-    }
-    private String toString(int index,LNode node,String s){
-	if(index==1){
-	    s+=node.getValue();
-	    return s;
+
+	private boolean remove(int index, LNode node) {
+		if (index == 1) {
+			node.setNext(node.getNext().getNext());
+			size -= 1;
+			return true;
+		}
+		return remove(index - 1, node.getNext());
 	}
-	s+=node.getValue()+", ";
-	toString(index-1,node.getNext(),s);
-    }
+
+	public int get(int index) {
+		return get(index, start);
+	}
+
+	private int get(int index, LNode node) {
+		if (index == 0) {
+			return node.getValue();
+		}
+		return get(index - 1, node.getNext());
+	}
+
+	public int set(int index, int newValue) {
+		return set(index, newValue, start);
+	}
+
+	private int set(int index, int value, LNode node) {
+		if (index == 0) {
+			return node.setValue(value);
+		}
+		return set(index - 1, value, node.getNext());
+	}
+
+	public int size() {
+		return size;
+	}
+
+	public int indexOf(int value) {
+		return indexOf(size, value, start);
+	}
+
+	private int indexOf(int index, int value, LNode node) {
+		if (index == 0) {
+			return -1;
+		}
+		if (node.getValue() == value) {
+			return size - index;
+		}
+		return indexOf(index - 1, value, node.getNext());
+	}
+
+	@Override
+	public String toString() {
+		return toString(size, start, "[ ") + " ]";
+	}
+
+	private String toString(int index, LNode node, String s) {
+		if (index == 1) {
+			s += node.getValue();
+			return s;
+		}
+		s += node.getValue() + ", ";
+		return toString(index - 1, node.getNext(), s);
+	}
 }
