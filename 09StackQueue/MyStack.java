9ddd70ecf4fd1b9c7b623d5a55df077bfd022003
MatthewHall-Pena <mhallpena@stuy.edu>
2016-03-29 13:02:25 -0400

    hw11-00

--- 09StackQueue/MyStack.java
+++ 09StackQueue/MyStack.java
@@ -1,16 +1,24 @@
-
+import java.util.*;
 public class MyStack<T> {
 	private MyLinkedList<T> list = new MyLinkedList<T>();
-
+	public MyStack(){
+	    list = new MyLinkedList<T>();
+	}
 	public void push(T value) {
 		list.add(0, value);
 	}
 
 	public T pop() {
+	    if(isEmpty()){
+		throw new NoSuchElementException();
+	    }
 		return list.remove(0);
 	}
 
 	public T peek() {
+	    if(isEmpty()){
+		throw new NoSuchElementException();
+	    }
 		return list.get(0);
 	}
 
---
+++ 09StackQueue/ParenDemo.java
@@ -0,0 +1,36 @@
+public class ParenDemo{
+    private static MyStack<Character> stack =new MyStack<Character>();
+    public static boolean isMatching(String s){
+	char[] chars=s.toCharArray();
+	for(int x=0;x<chars.length;x++){
+	    if(chars[x]=='('||chars[x]=='['||chars[x]=='{'){
+		stack.push(chars[x]);
+	    }
+	    if(!stack.isEmpty() && match(stack.peek(),chars[x])){
+		stack.pop();
+	    }
+	}
+	    return stack.isEmpty();
+    }
+    private static boolean match(char open,char close){
+	if(open=='('&&close==')'){
+	    return true;
+	}
+	if(open=='['&&close==']'){
+	    return true;
+	}
+	return open=='{'&&close=='}';	
+    }
+
+
+ public static void main(String[]args){
+     String input = "()()(([[]]))";
+ if(args.length > 0){
+    input = args[0];
+    System.out.println(isMatching(input)); 
+ }else{
+    System.out.println(isMatching(input)); 
+ }
+ 
+ }
+}
