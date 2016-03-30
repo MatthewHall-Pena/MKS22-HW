6c971b1cdacbe410eb1815d13080dfbb91e1dd87
MatthewHall-Pena <mhallpena@stuy.edu>
2016-03-30 13:08:44 -0400

    hw12-00

--- 09StackQueue/MyQueue.java
+++ 09StackQueue/MyQueue.java
@@ -1,7 +1,7 @@
 
+import java.util.*;
 public class MyQueue<T> {
 	private MyLinkedList<T> list = new MyLinkedList<T>();
-
 	public void enqueue(T value) {
 		list.add(value);
 	}
@@ -20,5 +20,19 @@ public class MyQueue<T> {
 
 	public boolean isEmpty() {
 		return list.size() == 0;
+	}
+
+	public static void main(String[]args){
+	    MyQueue<Double> queue =new MyQueue<Double>();
+	    Queue<Double> queue2 =new Queue<Double>();
+	    for(int x=0;x<10000;x++){
+		queue.enqueue(Math.random());
+		queue2.enqueue(Math.random());
+	    }
+	    for(int x=0;x<10000;x++){
+		queue.dequeue(Math.random());
+		queue2.dequeue(Math.random());
+	    }
+	    
 	}
 }
