b777116004f15f4731e6f876e89402a0c3ba306f
MatthewHall-Pena <mhallpena@stuy.edu>
2016-04-03 12:10:47 -0400

    hw14-00

---
+++ 10Deque/MyDeque.java
@@ -0,0 +1,97 @@
+import java.util.NoSuchElementException;
+
+public class MyDeque<T> {
+	T[] data;
+	int size = 0;
+	int start;
+	int end;
+
+	@SuppressWarnings("unchecked")
+	public MyDeque() {
+		data = (T[]) new Object[10];
+		start = data.length / 2;
+		end = start;
+	}
+
+	@SuppressWarnings("unchecked")
+	public MyDeque(int length) {
+		data = (T[]) new Object[length];
+		start = data.length / 2;
+		end = start;
+	}
+
+	@SuppressWarnings("unchecked")
+	private void grow() {
+		T[] ndata = (T[]) new Object[data.length * 2];
+		for (int x = 0; x < size; x++) {
+			ndata[x + ndata.length - size] = data[x];
+		}
+		data = ndata;
+	}
+
+	public void addFirst(T value) {
+		if (size == data.length) {
+			grow();
+		}
+		if (start - 1 >= 0) {
+			start -= 1;
+		} else {
+			start = data.length - 1;
+		}
+		data[start] = value;
+		size += 1;
+	}
+
+	public void addLast(T value) {
+		if (size == data.length) {
+			grow();
+		}
+		if (end + 1 < data.length) {
+			end += 1;
+		} else {
+			end = 0;
+		}
+		data[end] = value;
+		size += 1;
+	}
+
+	public T removeFirst() {
+		if (size <= 0) {
+			throw new NoSuchElementException();
+		}
+		size -= 1;
+		if (start + 1 < data.length) {
+			start += 1;
+			return data[start - 1];
+		}
+		start = 0;
+		return data[data.length - 1];
+	}
+
+	public T removeLast() {
+		if (size <= 0) {
+			throw new NoSuchElementException();
+		}
+		size -= 1;
+		if (end - 1 >= 0) {
+			end -= 1;
+			return data[end + 1];
+		}
+		end = data.length - 1;
+		return data[0];
+	}
+
+	public T getFirst() {
+		if (size <= 0) {
+			throw new NoSuchElementException();
+		}
+		return data[start];
+	}
+
+	public T getLast() {
+		if (size <= 0) {
+			throw new NoSuchElementException();
+		}
+		return data[end];
+	}
+}
